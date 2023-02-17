64108dbb406350b832b4e5679cb287d7ae280fe0
Justin Henry <jwhenry123@gmail.com>
2023-02-17 09:16:07 -0600

    Added skills by category grouping to repo

--- Plugins/Skill/SkillConfig.cs
+++ Plugins/Skill/SkillConfig.cs
@@ -8,6 +8,9 @@ namespace OpenFish.Plugins.Skill
     public class SkillConfig : ScriptableObject
     {
         public string Id;
+        // Use Category to group skills in a sane way, in order to
+        // display certain skills
+        public string Category;
         public string Name;
     }
 }
--- Plugins/Skill/SkillConfigRepo.cs
+++ Plugins/Skill/SkillConfigRepo.cs
@@ -10,16 +10,21 @@ namespace OpenFish.Plugins.Skill
     public class SkillConfigRepo : ScriptableObject
     {
         public Dictionary<string, SkillConfig> Skills;
+        public Dictionary<string, List<SkillConfig>> SkillsByCategory;
 
         private void OnEnable()
         {
             Skills = new();
+            SkillsByCategory = new();
             foreach (var guid in AssetDatabase.FindAssets("t:SkillConfig"))
             {
                 var path = AssetDatabase.GUIDToAssetPath(guid);
                 var asset = AssetDatabase.LoadAssetAtPath(path, typeof(SkillConfig)) as SkillConfig;
                 if (asset == null) continue;
+                if (!SkillsByCategory.ContainsKey(asset.Category))
+                    SkillsByCategory[asset.Category] = new();
                 Skills.Add(asset.Id, asset);
+                SkillsByCategory[asset.Category].Add(asset);
             }
             Debug.Log("Found " + Skills.Count + " Configs for Skills");
         }
