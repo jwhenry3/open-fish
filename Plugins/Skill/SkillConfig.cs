5bba333a48a859aab3cc5a8b23257df8516238aa
Justin Henry <jwhenry123@gmail.com>
2023-02-17 09:16:58 -0600

    Added more metadata to skill configs

--- Plugins/Skill/SkillConfig.cs
+++ Plugins/Skill/SkillConfig.cs
@@ -7,10 +7,13 @@ namespace OpenFish.Plugins.Skill
     [Serializable]
     public class SkillConfig : ScriptableObject
     {
+        // Short constant name to reference skills by
         public string Id;
         // Use Category to group skills in a sane way, in order to
         // display certain skills
         public string Category;
+        // More descriptive name
         public string Name;
+        public string Description;
     }
 }
